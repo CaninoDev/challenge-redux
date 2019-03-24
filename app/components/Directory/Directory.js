dd7be65d99d810b8ed21a704f9540bb95a0de4ae
IanGC <ian@thecommittedbug.io>
2019-03-24 12:42:47 -0400

    connect Directory component to redux

--- app/components/Directory/Directory.js
+++ app/components/Directory/Directory.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchDirectory } from '../../actions/directory';
 
-const Directory = () => {
-  return <div className="container">
-    <h1>Directory Placeholder</h1>
-  </div>
+class Directory extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      directory: this.props.data
+    };
+  }
+  componentDidMount () {
+    this.props.fetchDirectory();
+  }
+
+  componentWillReceiveProps(nextProps, nextContext) {
+    this.setState({
+      directory: nextProps.data
+    })
+  }
+
+  render() {
+    const { directory } = this.state;
+
+    return (
+       <div className="container">
+        {directory !== undefined && directory.map(student => <div key={student._id} />)}
+       </div>
+    )
+  }
 }
 
-export default Directory;
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchDirectory: () => dispatch(fetchDirectory())
+  };
+};
+
+const mapStateToProps = state => ({
+  data: state.directory.data
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Directory);
