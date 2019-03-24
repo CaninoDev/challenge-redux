26a0418b1ce929824092cfcf93e5b9813b2a7948
Ian G Canino <ian.canino@gmail.com>
2019-03-24 19:12:56 -0400

    render directory to a table

--- app/components/Directory/Directory.js
+++ app/components/Directory/Directory.js
@@ -2,6 +2,29 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchDirectory } from '../../actions/directory';
 
+
+const TableRow = props => {
+  const { data } = props;
+  const { firstname, lastname, email, age, grade } = data;
+  return (
+    <tr>
+      <td>{firstname}</td>
+      <td>{lastname}</td>
+      <td>{email}</td>
+      <td>{age}</td>
+      <td>{grade}</td>
+      <td>
+        <button type="button" className="btn btn-xs">
+          <span className="glyphicon glyphicon-pencil"></span>
+        </button>
+        <button type="button" className="btn btn-xs">
+          <span className="glyphicon glyphicon-trash"></span>
+        </button>
+      </td>
+    </tr>
+  )
+};
+
 class Directory extends React.Component{
   constructor(props) {
     super(props);
@@ -18,13 +41,40 @@ class Directory extends React.Component{
       directory: nextProps.data
     })
   }
-
   render() {
     const { directory } = this.state;
 
+    const fleshOutRow = (obj) => {
+      let webFragment = [];
+      obj.map(function(obj) { return [...Object.values(obj) ] } ).forEach(function(row) {webFragment.push(<td>{row}</td>)});
+      return [...webFragment];
+    }
+
     return (
        <div className="container">
-        {directory !== undefined && directory.map(student => <div key={student._id} />)}
+         <div className="panel">
+           <div className="panel-heading">
+             <h3 className="panel-title">Student Directory</h3>
+           </div>
+           <div className="panel-body">
+             <div className="table-responsive">
+             <table className="table table-hover table-condensed">
+               <thead>
+               <tr>
+                 <th>First Name</th>
+                 <th>Last Name</th>
+                 <th>eMail</th>
+                 <th>Age</th>
+                 <th>Grade</th>
+               </tr>
+               </thead>
+               <tbody>
+               {directory ? directory.map(student => <TableRow key={student._id} data={student}/>) : ""}
+               </tbody>
+               </table>
+             </div>
+           </div>
+           </div>
        </div>
     )
   }
