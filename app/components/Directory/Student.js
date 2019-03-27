3a16fb1f2ab28bdcf8571b6cab704e8d8a66ddc9
IanGC <ian@thecommittedbug.io>
2019-03-27 13:19:16 -0400

    refactor student info from directory component

--- app/components/Directory/Student.js
+++ app/components/Directory/Student.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Student = props => {
   const { data } = props;
-  const { firstname, lastname, email, age, grade } = data;
+  const { firstName, lastName, email, age, grade } = data;
   return (
      <tr>
-       <td>{firstname}</td>
-       <td>{lastname}</td>
-       <td>{email}</td>
-       <td>{age}</td>
-       <td>{grade}</td>
+       <td>{ firstName }</td>
+       <td>{ lastName }</td>
+       <td>{ email }</td>
+       <td>{ age }</td>
+       <td>{ grade }</td>
        <td>
          <button type="button" className="btn btn-warning btn-xs">E</button>
        </td>
